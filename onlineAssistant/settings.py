1d7f36d518517049b946c1e0cd2b4eea9123f2f5
turkbera <turk.mbera@gmail.com>
2023-09-18 17:23:08 +0300

    csrf_trusted_origins are added

--- onlineAssistant/settings.py
+++ onlineAssistant/settings.py
@@ -133,4 +133,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://personal-privacy-assistant.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://personal-privacy-assistant.up.railway.app']
