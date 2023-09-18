7d7fe7cc4cf0a941311335fbf66860954dfc41a8
turkbera <turk.mbera@gmail.com>
2023-09-18 16:52:53 +0300

    host name added to the settings.py

--- onlineAssistant/settings.py
+++ onlineAssistant/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mvxv2_o51rdjw^5p4!j-5wa-myhj%ewfkr_+2m&v9w^!n4h76_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['personal-privacy-assistant.up.railway.app']
 
 
 # Application definition
