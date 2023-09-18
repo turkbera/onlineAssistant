be5c07672b4866510e9fc93e694fd15f4c12b20a
turkbera <turk.mbera@gmail.com>
2023-09-18 16:21:22 +0300

    wsgi for railway

--- onlineAssistant/wsgi.py
+++ onlineAssistant/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlineAssistant.settings')
+
 application = get_wsgi_application()
-app = application
